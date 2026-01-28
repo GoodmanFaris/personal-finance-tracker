864e322bc8e36f81f39667b63117f41853e0e405
GoodmanFaris <flindov001@gmail.com>
2026-01-28 17:54:15 +0100

    added summary repo

--- backend/app/repositories/category.py
+++ backend/app/repositories/category.py
@@ -14,20 +14,23 @@ class CategoryRepository:
         return obj
     
     def list(self, *, user_id: int) -> List[Category]:
-        statement = select(Category).where(Category.user_id == user_id).order_by(Category.id)
+        statement = select(Category).where(Category.user_id == user_id, Category.active == True).order_by(Category.id)
         results = self.session.exec(statement).all()
         return results
 
     def get_by_id(self, *, category_id: int, user_id: int) -> Optional[Category]:
-        statement = select(Category).where(Category.id == category_id, Category.user_id == user_id)
+        statement = select(Category).where(Category.id == category_id, Category.user_id == user_id, Category.active == True)
         result = self.session.exec(statement).first()
         return result
     
     def get_by_name(self, *, name: str, user_id: int) -> Optional[Category]:
-        statement = select(Category).where(Category.name == name, Category.user_id == user_id)
+        statement = select(Category).where(Category.name == name, Category.user_id == user_id, Category.active == True)
         result = self.session.exec(statement).first()
         return result
 
-    def delete(self, category: Category) -> None:
-        self.session.delete(category)
+    def delete(self, *, category: Category) -> None:
+        category.active = False
+        self.session.add(category)
         self.session.commit()
+        self.session.refresh(category)
+        return category
---
+++ backend/app/repositories/summary.py
@@ -0,0 +1,67 @@
+from typing import Optional, List
+from sqlmodel import Session, select
+from app.models.income import Income
+from app.models.transaction import Transaction
+from app.models.user import User
+from app.models.category import Category
+
+
+class SummaryRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def get_income_by_range(self, *, user_id: int, start_month: str, end_month: str ) -> List[Income]:
+        statement = select(Income).where(
+            Income.user_id == user_id,
+            Income.month >= start_month,
+            Income.month <= end_month
+        ).order_by(Income.month)
+        results = self.session.exec(statement).all()
+        return results
+    
+    def get_transactions_by_range(self, *, user_id: int, start_month: str, end_month: str ) -> List[Transaction]:
+        statement = select(Transaction).where(
+            Transaction.user_id == user_id,
+            Transaction.month >= start_month,
+            Transaction.month <= end_month
+        ).order_by(Transaction.date)
+        results = self.session.exec(statement).all()
+        return results
+    
+    def get_expenses_by_category(self, *, user_id: int, start_month: str, end_month: str ) -> List[Transaction]:
+        statement = select(Transaction).where(
+            Transaction.user_id == user_id,
+            Transaction.month >= start_month,
+            Transaction.month <= end_month,
+            Transaction.type == "expense",
+            Transaction.category_id == Category.id
+        ).order_by(Transaction.date)
+        results = self.session.exec(statement).all()
+        return results
+    
+    def get_total_expenses(self, *, user_id: int, start_month: str, end_month: str ) -> float:
+        statement = select(Transaction).where(
+            Transaction.user_id == user_id,
+            Transaction.month >= start_month,
+            Transaction.month <= end_month,
+            Transaction.type == "expense"
+        )
+        results = self.session.exec(statement).all()
+        total_expenses = sum(transaction.amount for transaction in results)
+        return total_expenses
+    
+    def get_total_income(self, *, user_id: int, start_month: str, end_month: str ) -> float:
+        statement = select(Income).where(
+            Income.user_id == user_id,
+            Income.month >= start_month,
+            Income.month <= end_month
+        )
+        results = self.session.exec(statement).all()
+        total_income = sum(income.amount for income in results)
+        return total_income
+    
+    def get_money_saved(self, *, user_id: int, start_month: str, end_month: str ) -> float:
+        total_income = self.get_total_income(user_id=user_id, start_month=start_month, end_month=end_month)
+        total_expenses = self.get_total_expenses(user_id=user_id, start_month=start_month, end_month=end_month)
+        money_saved = total_income - total_expenses
+        return money_saved
