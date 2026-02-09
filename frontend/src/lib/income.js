6da1534a96a565b7f43167f08008418a24d7dd7a
GoodmanFaris <flindov001@gmail.com>
2026-02-09 22:13:34 +0100

    added axios fetch for every route.

---
+++ frontend/src/lib/income.js
@@ -0,0 +1,30 @@
+import api from "./api";
+
+export const getIncomes = () =>
+    api.get(`/income/${id}`).then(res => res.data);
+
+export const getIncomesByMonth = (month) =>
+    api.get(`/income/month/${month}`).then(res => res.data);
+
+export const listByTimePeriod = (startDate, endDate) =>
+    api.get(`/income/period?start_date=${startDate}&end_date=${endDate}`).then(res => res.data);
+
+export const createIncome = (data) =>
+    api.post("/income/", data);
+
+export const updateIncome = (id, data) =>
+    api.put(`/income/${id}`, data);
+
+export const updateIncomesByMonth = (month, data) =>
+    api.put(`/income/month/${month}`, data);
+
+export const deleteIncome = (id) =>
+    api.delete(`/income/${id}`);
+
+
+
+
+
+
+
+
