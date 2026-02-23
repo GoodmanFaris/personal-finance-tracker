9b79e9902dbfeb8d1645b7b40dc086fee7cf9823
GoodmanFaris <flindov001@gmail.com>
2026-02-23 14:00:55 +0100

    changed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,6 +20,9 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://personal-finance-tracker-ecru-kappa.vercel.app/",
+    "https://BudgetFlo.online,"
+
 ]
     
 app.add_middleware(
