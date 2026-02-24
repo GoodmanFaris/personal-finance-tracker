eaebaf199726791eb3243657525687ef9f454536
GoodmanFaris <flindov001@gmail.com>
2026-02-24 11:26:31 +0100

    added sitemap.ts for Google Search Console

---
+++ frontend/app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://budgetflo.online",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://budgetflo.online/login",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://budgetflo.online/register",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://budgetflo.online/about",
+      lastModified: new Date(),
+    },
+  ];
+}
